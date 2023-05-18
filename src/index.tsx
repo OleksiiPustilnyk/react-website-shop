595e46356dba5f76603d9fc44a29643bb7232b18
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2023-05-18 20:15:30 +0300

    add app title and paragraph

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 // React Element
-const title = React.createElement('h1', { id: 'title' }, 'Hello React')
+
+// const title = React.createElement('h1', { id: 'title' }, 'Hello React')
+// console.log(title)
+
+// JSX
+
+const app = (
+    <div>
+        <h1 id="text">Hello App</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus
+            iste fugit, recusandae consectetur, ratione porro libero magni
+            accusantium minima amet aperiam dolor, aliquam labore odio
+            consequatur sapiente! Soluta, error excepturi.
+        </p>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{title}</React.StrictMode>)
+root.render(<React.StrictMode>{app}</React.StrictMode>)
