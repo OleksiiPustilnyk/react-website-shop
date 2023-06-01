182f8099f300e887b8ea1f6d252b258467f5d027
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2023-06-01 19:10:19 +0300

    added key and id for productsArray

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -10,15 +10,21 @@ type Props = {
     price: number
 }
 
-const ProductListItem = (props: Props) => {
+const ProductListItem = ({
+    title,
+    description,
+    type,
+    capacity,
+    price,
+}: Props) => {
     return (
         <Card className="product" variant="outlined">
             <CardContent>
-                <h4 className="product-title">{props.title}</h4>
-                <p className="product-description">{props.description}</p>
-                <div className="product-features">Type: {props.type}</div>
-                <div className="product-features">{props.capacity}Gb</div>
-                <div className="product-price">$ {props.price}</div>
+                <h4 className="product-title">{title}</h4>
+                <p className="product-description">{description}</p>
+                <div className="product-features">Type: {type}</div>
+                <div className="product-features">{capacity}Gb</div>
+                <div className="product-price">$ {price}</div>
                 <CardActions className="btn-wrap">
                     <Button variant="outlined">Add to cart</Button>
                 </CardActions>
