650cc0fa56131cb892eb64979c4dc404023d7cba
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2023-06-22 19:00:32 +0300

    added likeSlice

--- src/redux/likeReducer.ts
+++ src/redux/likeReducer.ts
@@ -1,3 +1,4 @@
+import { createSlice } from '@reduxjs/toolkit'
 import { AnyAction } from 'redux'
 
 type LikeState = {
@@ -6,15 +7,30 @@ type LikeState = {
 
 const initialState: LikeState = { 1: true, 2: true }
 
-export const likeReducer = (state = initialState, action: AnyAction) => {
-    switch (action.type) {
-        case 'TOGGLE_LIKE': {
-            return {
-                ...state,
-                [action.id]: !state[action.id],
-            }
-        }
-        default:
-            return state
-    }
-}
+// export const likeReducer = (state = initialState, action: AnyAction) => {
+//     switch (action.type) {
+//         case 'TOGGLE_LIKE': {
+//             return {
+//                 ...state,
+//                 [action.id]: !state[action.id],
+//             }
+//         }
+//         default:
+//             return state
+//     }
+// }
+
+export const likeSlice = createSlice({
+    name: 'like',
+    initialState,
+    reducers: {
+        toggleLike: (state, action) => ({
+            ...state,
+            [action.payload]: !state[action.payload],
+        }),
+    },
+})
+
+export const { toggleLike } = likeSlice.actions
+
+export default likeSlice.reducer
