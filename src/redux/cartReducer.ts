87bf926086795f88d35817b3538cfd3dd26d9849
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2023-06-22 20:20:46 +0300

    edited removeProductFromCart

--- src/redux/cartReducer.ts
+++ src/redux/cartReducer.ts
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { omit } from 'lodash'
 
 type State = {
     [id: number]: number
@@ -12,7 +13,24 @@ const initialState: State = {
 export const cartSlice = createSlice({
     name: 'cart',
     initialState,
-    reducers: {},
+    reducers: {
+        addProductToCart: (state, action) => ({
+            ...state,
+            [action.payload.id]:
+                (state[action.payload.id] || 0) + action.payload.count,
+        }),
+        removeProductFromCart: (state, action) => omit(state, action.payload),
+        changeProductQuantity: (state, action) => ({
+            ...state,
+            [action.payload.id]: action.payload.count,
+        }),
+    },
 })
+
+export const {
+    addProductToCart,
+    removeProductFromCart,
+    changeProductQuantity,
+} = cartSlice.actions
 
 export default cartSlice.reducer
